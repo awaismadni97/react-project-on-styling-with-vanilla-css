0359ef780b059a85268b8fa86fe44adedbf2be56
Awais Madni <awais.madni17@gmail.com>
2024-08-07 01:19:21 +0500

    Migrating the Demo App to Tailwind Css

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -4,16 +4,6 @@ import { styled } from 'styled-components';
 import Button from './Button.jsx';
 import CustomInputLabel from './CustomInputLabel.jsx';
 
-const ControlContainer = styled.div`
- display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`
-
-// ControlContainer is written as CamelCase because it is know the custom component which is returning div
-//  on which the above styles have been applied
-
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
   const [enteredPassword, setEnteredPassword] = useState('');
@@ -35,8 +25,8 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div id="auth-inputs" className='w-full max-w-sm p-8 rounded mx-auto shadow-md bg-gradient-to-b from-stone-700 to-stone-800'>
+      <div className='flex flex-col gap-2 mb-6'>
         <CustomInputLabel
           type="email"
           invalid={emailNotValid}
@@ -55,8 +45,8 @@ export default function AuthInputs() {
             handleInputChange('password', event.target.value)
           }
         />
-      </ControlContainer>
-      <div className="actions">
+      </div>
+      <div className="flex justify-end gap-4">
         <button type="button" className="text-button">
           Create a new account
         </button>
